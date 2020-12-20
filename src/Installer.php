3c38f4c13d73ac9858aee145edfd8461446eca95
francoisvaillant <frvaillant@gmail.com>
2020-12-20 12:40:17 +0100

    test composer

--- src/Installer.php
+++ src/Installer.php
@@ -2,12 +2,12 @@
 
 
 namespace MapUx;
-
+use Composer\Script\Event;
 
 class Installer
 {
 
-    public static function installLeaflet()
+    public static function installLeaflet(Event $event)
     {
         shell_exec('npm install leaflet');
     }
