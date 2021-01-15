c5e47c0140707731e306f92e717eae4159b45a92
francoisvaillant <frvaillant@gmail.com>
2021-01-15 23:45:22 +0100

    finalize command with project dir provider

--- Command/ProjectDirProvider.php
+++ Command/ProjectDirProvider.php
@@ -13,15 +13,14 @@ class ProjectDirProvider
             $r = new \ReflectionClass($this);
 
             $dir = \dirname($r->getFileName());
-            $n = 1;
-            while ($n > 0) {
-                $n++;
+            $n = 0;
+            while ($n === 0) {
                 $dir = \dirname($dir);
                 if(is_file($dir.'/composer.json') && count(explode(self::AUTHOR, \dirname($dir))) === 1) {
-                    $n = 0;
+                    $n = 1;
                 }
-                if ($n > 50) {
-                    throw new \Exception('Impossible to determinate project directory');
+                if ($dir === \dirname($dir)) {
+                    return $dir;
                 }
             }
            return $dir;
