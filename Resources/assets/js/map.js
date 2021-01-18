a6152f66fe38a304c297db5093d0dc90d53a3b06
francoisvaillant <frvaillant@gmail.com>
2021-01-18 22:45:39 +0100

    change assets loading

--- Resources/assets/js/map.js
+++ Resources/assets/js/map.js
@@ -1,6 +1,16 @@
 import {MapBuilder} from "./MapBuilder";
-require ('leaflet/dist/leaflet.css')
+require('leaflet/dist/leaflet.css')
 import '../css/map.css'
+require('../images/marker-icon.png')
+require('../images/marker-shadow.png')
+require('../images/red-icon.png')
+require('../images/green-icon.png')
+require('../images/orange-icon.png')
+require('../images/yellow-icon.png')
+require('../images/pink-icon.png')
+require('../images/purple-icon.png')
+require('../images/brown-icon.png')
+require('../images/black-icon.png')
 
 if ("complete" === document.readyState || "interactive" === document.readyState) {
     if (document.getElementsByClassName('mapux-map').length > 0) {
