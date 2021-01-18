39253f36a84c347e5deec309ed00ff69bbe82f02
francoisvaillant <frvaillant@gmail.com>
2021-01-18 23:18:02 +0100

    refactor js

--- Resources/assets/js/map.js
+++ Resources/assets/js/map.js
@@ -1,17 +1,18 @@
 import {MapBuilder} from "./MapBuilder";
 require('leaflet/dist/leaflet.css')
 import '../css/map.css'
+
 export const reqIcons = {
-    "default": require ('../images/marker-icon.png'),
+    "default": require('../images/marker-icon.png'),
     "shadow": require('../images/marker-shadow.png'),
-    "red": require ('../images/red-icon.png'),
-    "green": require ('../images/green-icon.png'),
-    "orange": require ('../images/orange-icon.png'),
-    "yellow": require ('../images/yellow-icon.png'),
-    "pink": require ('../images/pink-icon.png'),
-    "purple": require ('../images/purple-icon.png'),
-    "brown": require ('../images/brown-icon.png'),
-    "black": require ('../images/black-icon.png'),
+    "red": require('../images/red-icon.png'),
+    "green": require('../images/green-icon.png'),
+    "orange": require('../images/orange-icon.png'),
+    "yellow": require('../images/yellow-icon.png'),
+    "pink": require('../images/pink-icon.png'),
+    "purple": require('../images/purple-icon.png'),
+    "brown": require('../images/brown-icon.png'),
+    "black": require('../images/black-icon.png'),
 }
 
 if ("complete" === document.readyState || "interactive" === document.readyState) {
