868cec779192932ef6352d509b6b2b724e857b38
francoisvaillant <frvaillant@gmail.com>
2021-01-19 11:04:35 +0100

    update tests

--- Model/IconsDataProvider.php
+++ Model/IconsDataProvider.php
@@ -1,9 +1,29 @@
 <?php
-
+    
 namespace MapUx\Model;
-
+        
 class IconsDataProvider {
 
-    const MAPUX_ICONS = [ ];
 
+    const MAPUX_ICONS = [
+        'build/app.css' => '/build/app.css',
+        'build/app.js' => '/build/app.js',
+        'build/runtime.js' => '/build/runtime.js',
+        'build/vendors~app.css' => '/build/vendors~app.css',
+        'build/vendors~app.js' => '/build/vendors~app.js',
+        'build/images/black-icon.png' => '/build/images/black-icon.39c79cc8.png',
+        'build/images/brown-icon.png' => '/build/images/brown-icon.c883e1f9.png',
+        'build/images/green-icon.png' => '/build/images/green-icon.c9f03329.png',
+        'build/images/layers-2x.png' => '/build/images/layers-2x.8f2c4d11.png',
+        'build/images/layers.png' => '/build/images/layers.416d9136.png',
+        'build/images/marker-icon.png' => '/build/images/marker-icon.2b3e1faf.png',
+        'build/images/marker-shadow.png' => '/build/images/marker-shadow.a0c6cc14.png',
+        'build/images/orange-icon.png' => '/build/images/orange-icon.e61cd608.png',
+        'build/images/pink-icon.png' => '/build/images/pink-icon.5d390743.png',
+        'build/images/purple-icon.png' => '/build/images/purple-icon.68351426.png',
+        'build/images/red-icon.png' => '/build/images/red-icon.c8f65a77.png',
+        'build/images/yellow-icon.png' => '/build/images/yellow-icon.7147e36b.png',
+        
+    ];
+            
 }
--- tests/Model/MapTest.php
+++ tests/Model/MapTest.php
@@ -7,6 +7,7 @@ use MapUx\Model\Marker;
 use MapUx\Model\Rectangle;
 use MapUx\Model\Circle;
 use PHPUnit\Framework\TestCase;
+use MapUx\Model\Layer;
 
 
 class MapTest extends TestCase
@@ -14,37 +15,37 @@ class MapTest extends TestCase
 
     public function testMap()
     {
-        $map = new \MapUx\Model\Map(44, 2, 10);
+        $map = new Map(44, 2, 10);
         $this->assertInstanceOf(Map::class, $map);
     }
 
     public function testGetCenterLatitude()
     {
-        $map = new \MapUx\Model\Map(44, 2, 10);
+        $map = new Map(44, 2, 10);
         $this->assertEquals(44.0, $map->getCenterLatitude());
     }
 
     public function testGetCenterLongitude()
     {
-        $map = new \MapUx\Model\Map(44, 2, 10);
+        $map = new Map(44, 2, 10);
         $this->assertEquals(2.0, $map->getCenterLongitude());
     }
 
     public function testGetZoomLevel()
     {
-        $map = new \MapUx\Model\Map(44, 2, 10);
+        $map = new Map(44, 2, 10);
         $this->assertEquals(10, $map->getZoomLevel());
     }
 
     public function testGetBackground()
     {
-        $map = new \MapUx\Model\Map(44, 2, 10);
-        $this->assertEquals(\MapUx\Model\Layer::DEFAULT_BACKGROUND, $map->getLayers()[0]->getBackground());
+        $map = new Map(44, 2, 10);
+        $this->assertEquals(Layer::DEFAULT_BACKGROUND, $map->getLayers()[0]->getBackground());
     }
 
     public function testLayersRectangle()
     {
-        $map = new \MapUx\Model\Map(44, 2, 10);
+        $map = new Map(44, 2, 10);
         $rectangle = new Rectangle([44,1], [42,0]);
         $map->addLayer($rectangle);
         $expected = '{"1":{"background":"","options":{"color":"#0d47a1","weight":2,"opacity":1,"fillColor":"#2196f3","fillOpacity":0.5},"isGeoJson":false,"events":null,"isRectangle":true,"points":[[44,1],[42,0]]}}';
@@ -55,7 +56,7 @@ class MapTest extends TestCase
 
     public function testLayersCircle()
     {
-        $map = new \MapUx\Model\Map(44.00, 2.00, 10);
+        $map = new Map(44.00, 2.00, 10);
         $circle = new Circle(44, 1, 10000);
         $map->addLayer($circle);
         $expected = '{"1":{"background":"","options":{"radius":10000,"color":"#0d47a1","weight":2,"opacity":1,"fillColor":"#2196f3","fillOpacity":0.5},"isGeoJson":false,"events":null,"isCircle":true,"center":[44,1]}}';
@@ -63,34 +64,5 @@ class MapTest extends TestCase
         $this->assertEquals(true, is_array(json_decode($expected, true)));
     }
 
-    public function testMarker()
-    {
-        $map = new \MapUx\Model\Map(44.00, 2.00, 10);
-        $marker = new Marker();
-        $map->addMarker($marker);
-        $expected = '[{"lat":44.8485138261124,"lon":-0.563934445381165,"icon":"{"iconUrl":"/bundle/mapux/images/marker-icon.png","shadowUrl":"/bundle/mapux/images/marker-shadow.png","iconSize":[25,41],"iconAnchor":[12,41],"popupAnchor":[1,-34],"tooltipAnchor":[16,-28],"shadowSize":[41,41],"className":""}","options":"null","popup":null,"events":null}]';
-        $this->assertEquals(1, count($map->getAllMarkers()));
-        $this->assertEquals($expected, stripslashes(stripslashes($map->getMarkers())));
-        $this->assertEquals(true, is_array(json_decode($map->getMarkers(), true)));
-    }
-
-    public function testGetPointsFromMarkers()
-    {
-        $map = new \MapUx\Model\Map(44.00, 2.00, 10);
-        $marker = new Marker(44, 1);
-        $map->addMarker($marker);
-        $marker2 = new Marker(44, 2);
-        $map->addMarker($marker2);
-        $marker3 = new Marker (44, 3);
-        $map->addMarker($marker3);
-
-        $excepted = [
-            [44, 1],
-            [44, 2],
-            [44, 3]
-        ];
-
-        $this->assertEquals($excepted, $map->getPointsFromMarkers());
-    }
 
 }
